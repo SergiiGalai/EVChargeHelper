99b54dc24bc3c1769fb35b79b379ef3fb44cf8e5
Sergii Galai <sergiy.galay@gmail.com>
2018-11-29 18:15:57 +0200

    optimized list of amperage for big values

--- app/src/main/java/com/example/dell/chargetimer/charge/ChargeValuesProvider.java
+++ app/src/main/java/com/example/dell/chargetimer/charge/ChargeValuesProvider.java
@@ -1,18 +1,39 @@
 package com.example.dell.chargetimer.charge;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class ChargeValuesProvider {
 
     public static List<String> getAllowedAmperage(int defaultAmperage){
-        List<String> values = generateSequence(6, 16, 2);
 
-        if (!values.contains(String.valueOf(defaultAmperage)))
-            values.add(String.valueOf(defaultAmperage));
+        List<String> values;
 
-        values.add("30");
-        values.add("32");
+        if (defaultAmperage > 16){
+            values = generateSequence(8, 16, 4);
+            List<String> additionalValues = generateSequence(defaultAmperage - 10, defaultAmperage + 10, 2);
+
+            for (String tmpValue : additionalValues) {
+                if (!values.contains(tmpValue))
+                    values.add(tmpValue);
+            }
+        }else{
+            values = generateSequence(6, 16, 2);
+
+            if (!values.contains(String.valueOf(defaultAmperage)))
+                values.add(String.valueOf(defaultAmperage));
+
+            values.add("32");
+        }
+
+        Collections.sort(values, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return Integer.parseInt(o1) - Integer.parseInt(o2);
+            }
+        });
 
         return values;
     }
