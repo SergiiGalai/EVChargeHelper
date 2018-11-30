f412d8db5b3bd9aa8efe377baacb582b83ec52dc
Sergii Galai <sergiy.galay@gmail.com>
2018-11-30 12:27:42 +0200

    updated charge formula

--- app/src/main/java/com/example/dell/chargetimer/charge/ChargeValuesProvider.java
+++ app/src/main/java/com/example/dell/chargetimer/charge/ChargeValuesProvider.java
@@ -1,33 +1,20 @@
 package com.example.dell.chargetimer.charge;
 
+import android.support.annotation.NonNull;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class ChargeValuesProvider {
+    private final static int MAX_HOME_SOCKET_AMPERAGE = 16;
 
     public static List<String> getAllowedAmperage(int defaultAmperage){
 
-        final int MaxHomeSocketAmperage = 16;
-        List<String> values;
-
-        if (defaultAmperage > MaxHomeSocketAmperage){
-            values = generateSequence(8, MaxHomeSocketAmperage, 4);
-            List<String> additionalValues = generateSequence(defaultAmperage - 10, defaultAmperage + 10, 2);
-
-            for (String tmpValue : additionalValues) {
-                if (!values.contains(tmpValue))
-                    values.add(tmpValue);
-            }
-        }else{
-            values = generateSequence(6, MaxHomeSocketAmperage, 2);
-
-            if (!values.contains(String.valueOf(defaultAmperage)))
-                values.add(String.valueOf(defaultAmperage));
-
-            values.add("32");
-        }
+        List<String> values = defaultAmperage > MAX_HOME_SOCKET_AMPERAGE
+            ? getAllowedAmperageForPublicChargers(defaultAmperage)
+            : getAllowedAmperageForHomeSockets(defaultAmperage);
 
         Collections.sort(values, new Comparator<String>() {
             @Override
@@ -39,17 +26,47 @@ public class ChargeValuesProvider {
         return values;
     }
 
-    public static List<String> getAllowedVoltage(int defaultVoltage){
-        return getAllowedVoltage(defaultVoltage, 40);
+    @NonNull
+    private static List<String> getAllowedAmperageForHomeSockets(int defaultAmperage) {
+        List<String> values;
+        values = generateSequence(6, MAX_HOME_SOCKET_AMPERAGE, 2);
+
+        if (!values.contains(String.valueOf(defaultAmperage)))
+            values.add(String.valueOf(defaultAmperage));
+
+        values.add("32");
+        return values;
     }
 
-    public static List<String> getAllowedVoltage(int defaultVoltage, int delta){
-        if (defaultVoltage < delta)
-            delta = defaultVoltage;
+    private static List<String> getAllowedAmperageForPublicChargers(int defaultAmperage) {
+        List<String> values;
+        values = generateSequence(8, MAX_HOME_SOCKET_AMPERAGE, 4);
+        List<String> additionalValues = generateSequence(defaultAmperage - 10, defaultAmperage + 10, 2);
 
+        for (String tmpValue : additionalValues) {
+            if (!values.contains(tmpValue))
+                values.add(tmpValue);
+        }
+        return values;
+    }
+
+    public static List<String> getAllowedVoltage(int defaultVoltage){
+        final int MAX_US_VOLTAGE = 140;
+        final int MAX_DELTA = 40;
+        final int DELTA_US = 20;
+
+        final int delta = defaultVoltage < MAX_DELTA
+                ? defaultVoltage : defaultVoltage < MAX_US_VOLTAGE
+                ? DELTA_US : MAX_DELTA;
+
+        return getAllowedVoltage(defaultVoltage, delta, 10);
+    }
+
+    private static List<String> getAllowedVoltage(int defaultVoltage, int delta, int step){
         int min = defaultVoltage - delta;
         int max = defaultVoltage + delta;
-        return generateSequence(min, max, 5);
+
+        return generateSequence(min, max, step);
     }
 
     private static List<String> generateSequence(int min, int max, int step){
