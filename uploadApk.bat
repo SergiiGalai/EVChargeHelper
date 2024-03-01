b06b7b120d58ed39e9d2a05363646eb4c09cfd50
gusenica <gusenica@gmail.com>
2024-03-01 22:50:05 +0100

    moved old project to the EvChargeHelper subfolder

--- uploadApk.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-for /f "tokens=1,2 delims==" %%a in (..\batchconfig.ini) do (
-if %%a==CloudStorage set RootDir=%%b
-)
-SET SrcPath=.\app\build\outputs\apk\debug
-SET TargetDir=%RootDir%\EVChargeHelper\output
-
-ECHO %TargetDir%
-XCOPY /Y %SrcPath%\*.apk %TargetDir%\chargeHelper.*
